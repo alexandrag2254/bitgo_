d3fa71910fe9b27e92ae37d46ad878852520967c
Alex <alexandrag2254@gmail.com>
2015-12-03 22:29:15 -0800

    fixed asynchronous issue

--- bitgo_code.js
+++ bitgo_code.js
@@ -7,96 +7,184 @@
 
 //*****************************************************************//
 
-
 var BitGo = require('bitgo');
-var bitgo = new BitGo.BitGo({ env: 'prod', accessToken: '7a53f9e70ae28a3254f685c01a2a958032dd239f9d31113e5e23ff434c131e33'});
+var bitgo = new BitGo.BitGo({ env: 'prod', accessToken: '17ec0eb485ddebd36ada6d9926e48cfe7755f0d1a44222fa4f7f8cb53b86b035'});
+
+function firstFunction(_callback){
+    // do some asynchronous work
 
 
-bitgo.ping({}, function(err,res){ console.dir(res); });
-
-bitgo.session({}, function(err,res){console.dir(res); });
-
-bitgo.wallets().createWalletWithKeychains({passphrase: 'changeme', label: 'apiwallet'}, 
-  function(err, res){ console.dir(err); wallet = res.wallet 
-	});
+    bitgo.ping({}, function(err,res){ console.dir(res); });
+    bitgo.session({}, function(err,res){console.dir(res); });
 
 
-var wallet;
+    setTimeout(Delay, 600);
 
-//-------***  wallet is undefined at this point  ****--------------//
+    function Delay() {
+      bitgo.wallets().createWalletWithKeychains({passphrase: 'changeme', label: 'apiwallet'},function(err, res){ console.dir(err); wallet = res.wallet;});
 
+      setTimeout(Delay2, 6000);
+      function Delay2(){
+        var wallet;
+        _callback();
+      }
+    }
 
-// Create a New Bitcoin Address
+}
 
-wallet.createAddress({ "chain": 0 }, function callback(err, address) {
-    console.dir(address);
-});
+function secondFunction(){
+    // call first function and pass in a callback function which
+    // first function runs when it has completed
+    firstFunction(function() {
 
+      console.log("wallet", wallet);
 
-//setting policy rule
+       wallet.createAddress({ "chain": 0 }, function callback(err, address) {console.dir(address);});
 
-var rule = {
-  id: "webhookRule1",
-  type: "webhook",
-  action: { type: "deny" },
-  condition: { "url": 'https://www.actuaries.org.uk/studying/exam-results/ca1-actuarial-risk-management' }
-};
-
-
-wallet.setPolicyRule(rule, function callback(err, w) { console.dir(w); });
-
-
-//code for the remote endpoint
-
-app.post('/name', function(req, res, next) {
-
- var webhookData = req.body;
-
- var theOutput;
-
- webhookData.outputs.forEach(function(output) {
-   if (output.outputWallet !== webhookData.walletId) {
-    theOutput = output.outputAddress; 
-	}
-
- });
+       var rule = { id: "webhookRule1",type: "webhook",action: { type: "deny" },condition: { "url": 'https://www.actuaries.org.uk/studying/exam-results/ca1-actuarial-risk-management' }};
 
 
-//url that we will be getting relevant data
- var url = "https://www.actuaries.org.uk/studying/exam-results/ca1-actuarial-risk-management";
+        wallet.setPolicyRule(rule, function callback(err, w) { console.dir(w); });
 
- var request = require('request');
- x = request.get(url, function (error, response, body) {
-   if (!error && response.statusCode == 200) {
 
-     var Data = JSON.parse(response.body);
-     console.dir(Data.results);
-     var name = '';
-     var outcome;
-     var address;
+        //code for the remote endpoint
 
-     Data.results.forEach(function(result) {
+        app.post('/name', function(req, res, next) {
 
-       if (result.name == "Banyard") {
-          name = result.value;
-          outcome = "name exists";
-          address = result.value;
-       } else{
-        outcome = "name does not exist";
-       }
-     });
+         var webhookData = req.body;
 
-     console.log('name: ' + name);
-     console.log('result: ' + outcome);
+         var theOutput;
 
-     if (theOutput === address && outcome == "name exists") {
-       res.status(200).send({});
-     } else {
-       res.status(400).send({});
-     }
-   } else {
-     console.log('error');
-     console.dir(error);
-   }
- });
-});
+         webhookData.outputs.forEach(function(output) {
+           if (output.outputWallet !== webhookData.walletId) {
+            theOutput = output.outputAddress; 
+          }
+
+         });
+
+
+        //url that we will be getting relevant data
+         var url = "https://www.actuaries.org.uk/studying/exam-results/ca1-actuarial-risk-management";
+
+         var request = require('request');
+         x = request.get(url, function (error, response, body) {
+           if (!error && response.statusCode == 200) {
+
+             var Data = JSON.parse(response.body);
+             console.dir(Data.results);
+             var name = '';
+             var outcome;
+             var address;
+
+             Data.results.forEach(function(result) {
+
+               if (result.name == "Banyard") {
+                  name = result.value;
+                  outcome = "name exists";
+                  address = result.value;
+               } else{
+                outcome = "name does not exist";
+               }
+             });
+
+             console.log('name: ' + name);
+             console.log('result: ' + outcome);
+
+             if (theOutput === address && outcome == "name exists") {
+               res.status(200).send({});
+             } else {
+               res.status(400).send({});
+             }
+           } else {
+             console.log('error');
+             console.dir(error);
+           }
+         });
+        });
+
+    });  
+
+
+}
+
+// function thirdFunction(){
+
+//     secondFunction(function(){
+//       console.log('huzzah, done with first function!');
+
+
+
+        //       wallet.createAddress({ "chain": 0 }, function callback(err, address) {console.dir(address);});
+
+
+        // //setting policy rule
+
+        // var rule = { id: "webhookRule1",type: "webhook",action: { type: "deny" },condition: { "url": 'https://www.actuaries.org.uk/studying/exam-results/ca1-actuarial-risk-management' }};
+
+
+        // wallet.setPolicyRule(rule, function callback(err, w) { console.dir(w); });
+
+
+        // //code for the remote endpoint
+
+        // app.post('/name', function(req, res, next) {
+
+        //  var webhookData = req.body;
+
+        //  var theOutput;
+
+        //  webhookData.outputs.forEach(function(output) {
+        //    if (output.outputWallet !== webhookData.walletId) {
+        //     theOutput = output.outputAddress; 
+        //   }
+
+        //  });
+
+
+        // //url that we will be getting relevant data
+        //  var url = "https://www.actuaries.org.uk/studying/exam-results/ca1-actuarial-risk-management";
+
+        //  var request = require('request');
+        //  x = request.get(url, function (error, response, body) {
+        //    if (!error && response.statusCode == 200) {
+
+        //      var Data = JSON.parse(response.body);
+        //      console.dir(Data.results);
+        //      var name = '';
+        //      var outcome;
+        //      var address;
+
+        //      Data.results.forEach(function(result) {
+
+        //        if (result.name == "Banyard") {
+        //           name = result.value;
+        //           outcome = "name exists";
+        //           address = result.value;
+        //        } else{
+        //         outcome = "name does not exist";
+        //        }
+        //      });
+
+        //      console.log('name: ' + name);
+        //      console.log('result: ' + outcome);
+
+        //      if (theOutput === address && outcome == "name exists") {
+        //        res.status(200).send({});
+        //      } else {
+        //        res.status(400).send({});
+        //      }
+        //    } else {
+        //      console.log('error');
+        //      console.dir(error);
+        //    }
+        //  });
+        // });
+
+//     });   
+    
+//     }
+
+//running the full set of functions
+secondFunction();
+// thirdFunction();
+
